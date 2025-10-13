87ec2f22bec3adeb293d1dd76f3ca6dae8f26758
Heath Raftery <heath@empirical.ee>
2025-10-14 00:37:38 +1100

    Painstaking first pass
    Spent forever on serialisation and temporary file creation. Total nightmare.

--- layout_stamp.py
+++ layout_stamp.py
@@ -1,60 +1,107 @@
 #!/usr/bin/env python3
 
-from typing import cast
+# Icon from flaticon.com: <a href="https://www.flaticon.com/free-icons/clone-stamp" title="clone stamp icons">Clone stamp icons created by Freepik - Flaticon</a>
 
 from kipy import KiCad
 from kipy.geometry import Vector2
 from kipy.board import BoardLayer, BoardLayerClass
-from kipy.board_types import BoardText, FootprintInstance
+from kipy.board_types import BoardText, FootprintInstance, Field
+
+import sys
+import os
+import tempfile
+import pickle
+import shutil
+#import kipy.proto.common.types.base_types_pb2
+#import json
+
+def copy_selected_footprint_properties():
+    selection = board.get_selection()
+    if len(selection) != 1:
+        print("Select a single footprint first.")
+        sys.exit()
+
+    # TODO: Very hard to serialise a dict of KiCad properties.
+    #       Instead, serialise the whole selection and extract properties later.
+    #return board.get_selection_as_string()
+    return selection[0]._proto.SerializeToString()
+
+    s = selection[0]
+    props = {}
+    props['position'] = s.position._proto.SerializeToString()
+    props['orientation'] = s.orientation._proto.SerializeToString()
+    # We're assuming we only want to copy the footprint instance properties, so ignore
+    # free text objects, which are a property of the footprint in the library.
+    props['fields'] = [f._proto.SerializeToString() for f in s.texts_and_fields if isinstance(f, Field)]
+
+    return props
+
+def get_properties_from_serialised_footprint(ser):
+    fpi = FootprintInstance()
+    fpi._proto.ParseFromString(ser)
+    
+    props = {}
+    props['position'] = fpi.position
+    props['orientation'] = fpi.orientation
+    # We're assuming we only want to copy the footprint instance properties, so ignore
+    # free text objects, which are a property of the footprint in the library.
+    props['fields'] = [f for f in fpi.texts_and_fields if isinstance(f, Field)]
+
+    return props
+
+def paste_footprint_properties(props):
+    selection = board.get_selection()
+    if len(selection) != 1:
+        print("Select a single footprint first.")
+        sys.exit()
+
+    s = selection[0]
+
+    offset = s.position - props['position']
+    
+    commit = board.begin_commit() # do all changes in one undo history step
+    s.orientation = props['orientation']
+    for f_src in props['fields']:
+        for f_dst in [f for f in s.texts_and_fields if isinstance(f, Field)]:
+            if f_src.name == f_dst.name: 
+                f_dst.visible = f_src.visible
+                f_dst.layer = f_src.layer
+                f_dst.text.attributes = f_src.text.attributes # argh, where's knockout?
+                f_dst.text.layer = f_src.text.layer
+                f_dst.text.position = f_src.text.position + offset
+                break
+    board.push_commit(commit, 'stamp layout') # NOTE: I don't know where the name appears
+
+    board.update_items(s)
 
 
 if __name__=='__main__':
     kicad = KiCad()
     board = kicad.get_board()
-    stackup = board.get_stackup()
-    defaults = board.get_graphics_defaults()[BoardLayerClass.BLC_COPPER]
 
-    sizing_text = BoardText()
-    sizing_text.layer = BoardLayer.BL_F_Cu
-    sizing_text.position = Vector2.from_xy(0, 0)
-    sizing_text.value = "0"
-    sizing_text.attributes = defaults.text
+    # tmp_dir = tempfile.mkdtemp()
+    # with tempfile.TemporaryDirectory(dir='copy-layout') as tmp_dir:
+    #     tmp_path = os.path.join(tmp_dir, 'clipboard')
+        #tmp_path = tmp_dir + 'clipboard'
+    #shutil.copy(tmp_dir.name, 'bar.txt')
 
-    char_width = kicad.get_text_extents(sizing_text.as_text()).size.x
+    tmp_dir = kicad.get_plugin_settings_path('copy-layout')
+    os.makedirs(tmp_dir, exist_ok=True)
+    tmp_path = os.path.join(tmp_dir, 'clipboard')
+    #tmp_path.mkdir(parents=True, exist_ok=True)
 
-    copper_layers = [layer for layer in stackup.layers
-                     if layer.layer <= BoardLayer.BL_B_Cu
-                     and layer.layer >= BoardLayer.BL_F_Cu]
 
-    fpi = FootprintInstance()
-    fpi.layer = BoardLayer.BL_F_Cu
-    fpi.reference_field.text.value = "STACKUP1"
-    fpi.reference_field.text.attributes = defaults.text
-    fpi.reference_field.visible = False
-    fpi.value_field.text.attributes = defaults.text
-    fpi.value_field.visible = False
-    fpi.attributes.not_in_schematic = True
-    fpi.attributes.exclude_from_bill_of_materials = True
-    fpi.attributes.exclude_from_position_files = True
-    fp = fpi.definition
-
-    offset = 0
-    layer_idx = 1
-    for copper_layer in copper_layers:
-        layer_text = BoardText()
-        layer_text.layer = copper_layer.layer
-        layer_text.value = "%d" % layer_idx
-        layer_text.position = Vector2.from_xy(offset, 0)
-        layer_text.attributes = defaults.text
-        fp.add_item(layer_text)
-
-        padding = 1 if layer_idx == 9 else 0.5
-        item_width = int((len(layer_text.value) + padding) * char_width)
-
-        offset += item_width
-        layer_idx += 1
-
-    created = [cast(FootprintInstance, i) for i in board.create_items(fpi)]
-
-    if len(created) == 1:
-        board.interactive_move(created[0].id)
+    if len(sys.argv) >= 2 and sys.argv[1] == 'copy':
+        with open(tmp_path, 'wb') as f:
+            props = copy_selected_footprint_properties()
+#            pickle.dump(props, f)
+#            json.dump(props, f)
+            f.write(props)
+    elif len(sys.argv) >= 2 and sys.argv[1] == 'paste':
+        with open(tmp_path, 'rb') as f:
+#            props = pickle.load(f)
+#            props = json.load(f)
+            props = get_properties_from_serialised_footprint(f.read())
+            paste_footprint_properties(props)
+    else:
+        print("Must pass 'copy' or 'paste' as argument.")
